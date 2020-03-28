7aa40c9260f43b10d2547fd65980133e5b2db28f
SpiritSeeker <sanjeet029@gmail.com>
2020-03-28 17:01:49 +0530

    Cleaner premake

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +21,6 @@ project "GLFW"
 
 	filter "system:linux"
 		buildoptions { "-std=c11", "-fPIC" }
-		staticruntime "On"
 
 		files
 		{
@@ -42,9 +42,7 @@ project "GLFW"
 		}
 
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
@@ -65,5 +63,10 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
